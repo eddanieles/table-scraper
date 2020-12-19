5a7426c17a9fcfdd0024f0a9e7a01eb6b94a6225
eddanieles <eddanieles@gmail.com>
2020-12-19 04:47:41 -0500

    input values

--- src/components/Scraper.vue
+++ src/components/Scraper.vue
@@ -8,10 +8,27 @@
             name="url"
             label="What is the url of the site you want to scrap?"
         />
-        <FormulateInput
-            name="path"
-            label="Which data did you want to grab?"
-        />
+            <FormulateInput
+                name="tblData"
+                type="group"
+                :repeatable="true"
+                label="Which table data to grab?"
+                add-label="+ Add"
+                validation="required"
+            >
+                <FormulateInput
+                    name="name"
+                    label="Name:"
+                />
+                <FormulateInput
+                    name="path"
+                    label="Path:"
+                />
+                <FormulateInput
+                    name="attrib"
+                    label="Attribute:"
+                />
+            </FormulateInput>
         <FormulateInput
             type="submit"
             label="Get Data"
@@ -26,8 +43,8 @@
 <script>
 import rp from 'request-promise'
 import $ from 'cheerio'
-import * as fb from '../firebase'
-import _ from 'lodash'
+// import * as fb from '../firebase'
+// import _ from 'lodash'
 
 export default {
     data() {
@@ -41,37 +58,37 @@ export default {
             let that = this;
             rp("https://cors-anywhere.herokuapp.com/" + this.formValues.url)
                 .then(function(html) {
-                    let dataLength = $('#mw-content-text > div.mw-parser-output > table:nth-child(12) > tbody > tr > td:nth-child(3) > span > span > span > a', html).length
                     //success!
                     const players = [];
-                    for (let i = 0; i < dataLength; i++) {
-                        let name = $('#mw-content-text > div.mw-parser-output > table:nth-child(12) > tbody > tr > td:nth-child(3) > span > span > span > a', html)[i].attribs.title;
-                        let url = $('#mw-content-text > div.mw-parser-output > table:nth-child(12) > tbody > tr > td:nth-child(3) > span > span > span > a', html)[i].attribs.href;
-                        players.push({
-                            name,
-                            url
-                        })
-                    }
-                    that.data = players
-                    return players
+                    that.formValues.tblData.map(column => {
+                        let dataLength = $(column.path, html).length;
+                        for (let i = 0; i < dataLength; i++) {
+                            // let url = $('#mw-content-text > div.mw-parser-output > table:nth-child(12) > tbody > tr > td:nth-child(3) > span > span > span > a', html)[i].attribs.href;
+                            players[i] = {}
+                            players[i][column.name] = $(column.path, html)[i].attribs[column.attrib]
+                        }
+                    })
+                    
+                    that.data = players;
+                    return players;
                 })
-                .then(players => {
-                    players.map(player => {
+                // .then(players => {
+                //     players.map(player => {
 
-                        fb.playersCollection.get()
-                            .then(querySnapshot => {
-                                querySnapshot.forEach(async doc => {
-                                    if (!_.find(players, doc.data()) && player != doc.data()) {
-                                        console.log(`player: ${JSON.stringify(player)} firebase doc: ${JSON.stringify(doc.data())}`)
-                                        await fb.playersCollection.add({
-                                            name: player.name,
-                                            url: player.url
-                                        })
-                                    }
-                                })
-                            })
-                    })                    
-                })
+                //         fb.playersCollection.get()
+                //             .then(querySnapshot => {
+                //                 querySnapshot.forEach(async doc => {
+                //                     if (!_.find(players, doc.data()) && player != doc.data()) {
+                //                         // console.log(`player: ${JSON.stringify(player)} firebase doc: ${JSON.stringify(doc.data())}`)
+                //                         await fb.playersCollection.add({
+                //                             name: player.name,
+                //                             url: player.url
+                //                         })
+                //                     }
+                //                 })
+                //             })
+                //     })                    
+                // })
                 .catch(function(err) {
                     //handle error
                     console.error(err);
